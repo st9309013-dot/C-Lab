3efeab91b7942b8e2e9a01a71c0cc54d8ac132f6
st9309013-dot <st9309013@stud.duikt.edu.ua>
2026-04-02 11:31:05 +0300

    60

---
+++ Modul task2/Modul task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace Modul_task2
+{
+    public class MessagePublisher
+    {
+        public event Action<string> MessageSent;
+
+        public void Send(string message)
+        {
+            MessageSent?.Invoke(message);
+        }
+    }
+
+    public class FileLogger
+    {
+        private readonly string _logFilePath;
+
+        public FileLogger(string logFilePath, MessagePublisher publisher)
+        {
+            _logFilePath = logFilePath;
+            publisher.MessageSent += LogToFile;
+        }
+
+        private void LogToFile(string message)
+        {
+            string logEntry = $"[{DateTime.Now:HH:mm:ss}] {message}\n";
+            File.AppendAllText(_logFilePath, logEntry);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectDir = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"));
+            string logFile = Path.Combine(projectDir, "log24.txt");
+
+            MessagePublisher publisher = new MessagePublisher();
+            FileLogger logger = new FileLogger(logFile, publisher);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Введіть текст: ");
+                string input = Console.ReadLine();
+                publisher.Send(input);
+            }
+        }
+    }
+}
